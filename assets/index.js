98872138d7d65ec2aac937a107842acfbf21250b
andrewprog97 <andrewprog97@gmail.com>
2022-11-20 13:29:18 +0300

    add getTimetable emitter

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,28 @@
 (async () => {
+    const groupSelect = document.querySelector(".group-select");
+    const weekDaySelect = document.querySelector(".weekday-select");
+
+    const fetchingTableFunction = async () => {
+        if (!groupSelect.value || !weekDaySelect.value) return 0;
+        let answer = await fetch("/getTimetable", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ groupId: groupSelect.value, weekDay: weekDaySelect.value })
+        }).then(res => res.json());
+        console.log(answer);
+    };
+
     let groupsList = await fetch("/getGroupsList").then(res => res.json());
     groupsList.groups.forEach((e) => {
         let option = document.createElement("option");
         option.value = e;
         option.innerText = e;
-        document.querySelector(".group-select").appendChild(option);
+        groupSelect.appendChild(option);
     });
+
+    groupSelect.addEventListener("change", fetchingTableFunction);
+    weekDaySelect.addEventListener("change", fetchingTableFunction);
+
 })();
--- main.js
+++ main.js
@@ -15,8 +15,22 @@ app.get("/getGroupsList", async (req, res) => {
     await client.connect();
     const db = client.db("universitytable");
     const groupsCollection = db.collection("groups");
-    const groups = await groupsCollection.find({}).toArray();
-    res.send({groups: (await groupsCollection.find({}).toArray()).map((e) => e.groupId)});
+    res.send({ groups: (await groupsCollection.find({}).toArray()).map((e) => e.groupId) });
+    client.close();
+});
+
+app.post("/getTimetable", async (req, res) => {
+    await client.connect();
+    const db = client.db("universitytable");
+    const groupSubjects = db.collection("group" + req.body.groupId);
+    const commonTimetable = db.collection("commonTimetable");
+    res.send({ timetable: (await groupSubjects.find({ weekDay: req.body.weekDay }).toArray()).map(e => {
+        delete e._id;
+        return e;
+    }), commonTimetable: (await commonTimetable.find({}).toArray()).map(e => {
+        delete e._id;
+        return e;
+    }) });
     client.close();
 });
 
